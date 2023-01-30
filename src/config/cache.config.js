3f16009dc68b289d04ca87e65e501da5bd30a05b
bolaxd <iqbalsheila619@gmail.com>
2023-01-31 04:35:18 +0700

    ngupdet

--- src/config/cache.config.js
+++ src/config/cache.config.js
@@ -2,4 +2,9 @@ import { EventEmitter } from "events";
 
 export const Metadata = new Map();
 
+/**
+ * GW kasih Infinity kenapa?
+ * ya karna cache disini bakal nyimpen banyak data
+ * gw gamau nantinya bakal ada pembatasan dari Nodejs itu sendiri
+*/
 EventEmitter.defaultMaxListeners = Infinity;
